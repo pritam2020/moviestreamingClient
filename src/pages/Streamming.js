446c09582cdd412426afbd26dbc16229b469e0f8
pritam2020 <pritam20002001@gmail.com>
2024-09-01 04:26:41 +0530

    styled the movie details && added a gradient background to streamming page , layout.js and to the home page

--- src/pages/Streamming.js
+++ src/pages/Streamming.js
@@ -49,11 +49,23 @@ const Streaming = () => {
 
   return (
     <div className="streamming-container">
-      <h1 className="movie-title">
-        {streammingData && streammingData.MovieName
-          ? streammingData.MovieName
-          : "no movie name found"}
-      </h1>
+      <div className="movie-title-container">
+        <div style={{padding:'20px',marginLeft:'20px'}}>
+          <h1 className="movie-title">
+            {streammingData && streammingData.MovieName
+              ? streammingData.MovieName
+              : "no movie name found"}
+          </h1>
+        </div>
+        <div style={{padding:'20px'}}> </div>
+        <div style={{padding:'20px'}}> </div>
+        <div style={{padding:'20px'}}> </div>
+        <div style={{padding:'20px'}}> </div>
+        <div style={{padding:'20px'}}> </div>
+        <div style={{padding:'20px'}}> </div>
+        <div style={{padding:'20px'}}> </div>
+      </div>
+      <br />
       <video width="1000px" height="auto" controls>
         <source
           src={`https://localhost:3002/protected-route/videos/${
@@ -64,42 +76,52 @@ const Streaming = () => {
           type="video/mp4"
         />
       </video>
-      <h2 className="description-title">Description</h2>
-      <h4 className="description">
-        {streammingData && streammingData.MovieDescription
-          ? streammingData.MovieDescription
-          : "description not found"}
-      </h4>
-      <h2 className="cast-title">Cast</h2>
-      <h4 className="cast">
-        {streammingData && streammingData.cast
-          ? streammingData.cast
-          : "cast not found"}
-      </h4>
-      <img className="imdb-icon" src={imdbIcon} />
-      <h4 className="imdb-rating">
-        {streammingData && streammingData.IMDBRating
-          ? streammingData.IMDBRating
-          : "IMDB Rating not found"}
-      </h4>
-      <h2 className="directors-title">Directors</h2>
-      <h4 className="directors">
-        {streammingData && streammingData.Directors
-          ? streammingData.Directors
-          : "no directors found"}
-      </h4>
-      <h2 className="language-title">Language </h2>
-      <h4 className="language">
-        {streammingData && streammingData.Language
-          ? streammingData.Language
-          : "no language  found"}
-      </h4>
-      <h2 className="releaseDate-title">Release Date </h2>
-      <h4 className="releaseDate">
-        {streammingData && streammingData.ReleaseDate
-          ? streammingData.ReleaseDate
-          : "no release date found"}
-      </h4>
+      <div className="movie-info-flex-container">
+        <div className="left-flex-container">
+          <h2 className="description-title">Description</h2>
+          <h4 className="description">
+            {streammingData && streammingData.MovieDescription
+              ? streammingData.MovieDescription
+              : "description not found"}
+          </h4>
+          <img className="imdb-icon" src={imdbIcon} />
+          <h4 className="imdb-rating">
+            {streammingData && streammingData.IMDBRating
+              ? streammingData.IMDBRating
+              : "IMDB Rating not found"}
+          </h4>
+        </div>
+        <div className="right-flex-container">
+          <h2 className="cast-title">Cast</h2>
+          <h4 className="cast">
+            {streammingData && streammingData.cast
+              ? streammingData.cast
+              : "cast not found"}
+          </h4>
+          <br />
+          <h2 className="directors-title">Directors</h2>
+          <h4 className="directors">
+            {streammingData && streammingData.Directors
+              ? streammingData.Directors
+              : "no directors found"}
+          </h4>
+          <br />
+          <h2 className="language-title">Language </h2>
+          <h4 className="language">
+            {streammingData && streammingData.Language
+              ? streammingData.Language
+              : "no language  found"}
+          </h4>
+          <br />
+          <h2 className="releaseDate-title">Release Date </h2>
+          <h4 className="releaseDate">
+            {streammingData && streammingData.ReleaseDate
+              ? streammingData.ReleaseDate
+              : "no release date found"}
+          </h4>
+        </div>
+      </div>
+      <br />
     </div>
   );
 };
