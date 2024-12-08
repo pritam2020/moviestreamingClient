4b4d42b84ca9479703df19bec9977f811dde00d7
pritam2020 <pritam20002001@gmail.com>
2024-12-09 04:29:54 +0530

    deployement 12

--- src/App.js
+++ src/App.js
@@ -20,7 +20,6 @@ function App() {
             <Router>
             <ScrollToTop/>
                 <Routes>
-                   
                     <Route path="/" element={<Login/>} />
                     <Route path="/signup"  element={<Signup/>} />
                     <Route path="/user"  element={<Layout/>}>
